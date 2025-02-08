496733267ec8e2eda9a574e84249c3645a2a3a6c
varubogu <toyosuke@varubogu.com>
2025-02-08 06:16:00 +0000

    configの設定見直し

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
 
 import tailwindcss from '@tailwindcss/vite';
 
@@ -9,14 +8,8 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   integrations: [
     react(),
-
   ],
   vite: {
-    // Bunを使用する設定
-    server: {
-      force: true
-    },
-
     plugins: [tailwindcss()]
   }
 });
