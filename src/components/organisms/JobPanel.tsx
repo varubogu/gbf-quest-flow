5e2653201c5963c878e9695eeadc56ebda0793db
varubogu <toyosuke@varubogu.com>
2025-02-21 18:56:59 +0000

    リファクタ

--- src/components/organisms/JobPanel.tsx
+++ src/components/organisms/JobPanel.tsx
@@ -15,6 +15,57 @@ import {
 } from '@/components/atoms/TableStyles';
 import { useAutoResizeTextArea } from '@/hooks/useAutoResizeTextArea';
 
+// アビリティ行コンポーネント
+const AbilityRow: React.FC<{
+  ability: JobAbility;
+  index: number;
+  isEditing: boolean;
+  totalAbilities: number;
+  onAbilityChange: (_index: number, _field: keyof JobAbility, _value: string) => void;
+}> = ({ ability, index, isEditing, totalAbilities, onAbilityChange }) => {
+  const { t } = useTranslation();
+  const abilityNoteRef = useAutoResizeTextArea(ability.note);
+
+  return (
+    <tr>
+      {index === 0 && (
+        <td className={tableCellBaseStyle} rowSpan={totalAbilities}>
+          {t('characterAbilities')}
+        </td>
+      )}
+      <td className={tableCellBaseStyle}>
+        {isEditing ? (
+          <input
+            type="text"
+            value={ability.name}
+            onChange={(e) => onAbilityChange(index, 'name', e.target.value)}
+            className={textInputBaseStyle}
+          />
+        ) : (
+          ability.name
+        )}
+      </td>
+      <td className={tableCellBaseStyle}>
+        {isEditing ? (
+          <textarea
+            ref={abilityNoteRef}
+            value={ability.note}
+            onChange={(e) => onAbilityChange(index, 'note', e.target.value)}
+            className={textareaBaseStyle}
+          />
+        ) : (
+          ability.note.split('\n').map((line, i) => (
+            <React.Fragment key={i}>
+              {line}
+              {i < ability.note.split('\n').length - 1 && <br />}
+            </React.Fragment>
+          ))
+        )}
+      </td>
+    </tr>
+  );
+};
+
 interface JobPanelProps {
   isEditing: boolean;
 }
@@ -161,50 +212,16 @@ export const JobPanel: React.FC<JobPanelProps> = ({ isEditing }) => {
             </td>
           </tr>
           {/* アビリティ行 */}
-          {flowData.organization.job.abilities.map((ability, index) => {
-            const abilityNoteRef = useAutoResizeTextArea(ability.note);
-            return (
-              <tr key={`ability-${index}`}>
-                {index === 0 && (
-                  <td
-                    className={tableCellBaseStyle}
-                    rowSpan={flowData.organization.job.abilities.length}
-                  >
-                    {t('characterAbilities')}
-                  </td>
-                )}
-                <td className={tableCellBaseStyle}>
-                  {isEditing ? (
-                    <input
-                      type="text"
-                      value={ability.name}
-                      onChange={(e) => handleAbilityChange(index, 'name', e.target.value)}
-                      className={textInputBaseStyle}
-                    />
-                  ) : (
-                    ability.name
-                  )}
-                </td>
-                <td className={tableCellBaseStyle}>
-                  {isEditing ? (
-                    <textarea
-                      ref={abilityNoteRef}
-                      value={ability.note}
-                      onChange={(e) => handleAbilityChange(index, 'note', e.target.value)}
-                      className={textareaBaseStyle}
-                    />
-                  ) : (
-                    ability.note.split('\n').map((line, i) => (
-                      <React.Fragment key={i}>
-                        {line}
-                        {i < ability.note.split('\n').length - 1 && <br />}
-                      </React.Fragment>
-                    ))
-                  )}
-                </td>
-              </tr>
-            );
-          })}
+          {flowData.organization.job.abilities.map((ability, index) => (
+            <AbilityRow
+              key={`ability-${index}`}
+              ability={ability}
+              index={index}
+              isEditing={isEditing}
+              totalAbilities={flowData.organization.job.abilities.length}
+              onAbilityChange={handleAbilityChange}
+            />
+          ))}
         </tbody>
       </table>
     </div>
