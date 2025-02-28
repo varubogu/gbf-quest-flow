1878d6b1e371a662aba1fd8f887ed98f0b81ae51
varubogu <toyosuke@varubogu.com>
2025-02-28 04:57:20 +0000

    eslitの設定でjsファイルまでtypescriptルールの対象となっていたのを修正

--- eslint.config.js
+++ eslint.config.js
@@ -56,26 +56,10 @@ const config = [
       },
     },
     plugins: {
-      '@typescript-eslint': /** @type {import('@typescript-eslint/utils').TSESLint.Plugin} */ (tseslint),
       react: /** @type {import('eslint-plugin-react').Plugin} */ (reactPlugin),
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       'react-hooks': /** @type {import('eslint').ESLint.Plugin} */ (reactHooksPlugin),
     },
     rules: {
-      '@typescript-eslint/explicit-function-return-type': 'warn',
-      '@typescript-eslint/explicit-module-boundary-types': 'warn',
-      '@typescript-eslint/no-unused-vars': ['error', {
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
-        ignoreRestSiblings: true,
-        destructuredArrayIgnorePattern: '^_',
-        caughtErrorsIgnorePattern: '^_',
-        args: 'all'
-      }],
-      '@typescript-eslint/no-explicit-any': 'error',
-      '@typescript-eslint/no-unsafe-assignment': 'warn',
-      '@typescript-eslint/no-unsafe-member-access': 'warn',
-      '@typescript-eslint/no-unsafe-call': 'warn',
       'react/react-in-jsx-scope': 'off',
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn',
@@ -101,15 +85,66 @@ const config = [
   {
     files: ['**/*.test.{ts,tsx}', '**/*.spec.{ts,tsx}'],
     languageOptions: {
+      parser: tseslintParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+
+    },
+  },
+  // TypeScript
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tseslintParser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
       globals: {
-        jest: 'readonly',
-        describe: 'readonly',
-        it: 'readonly',
-        expect: 'readonly',
-        beforeEach: 'readonly',
-        afterEach: 'readonly',
-        beforeAll: 'readonly',
-        afterAll: 'readonly',
+        window: 'readonly',
+        document: 'readonly',
+        console: 'readonly',
+        process: 'readonly',
+        setTimeout: 'readonly',
+        clearTimeout: 'readonly',
+        setInterval: 'readonly',
+        clearInterval: 'readonly',
+        fetch: 'readonly',
+        alert: 'readonly',
+        confirm: 'readonly',
+        history: 'readonly',
+        structuredClone: 'readonly',
+        __dirname: 'readonly',
+        require: 'readonly',
+        React: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': /** @type {import('@typescript-eslint/utils').TSESLint.Plugin} */ (tseslint),
+      react: /** @type {import('eslint-plugin-react').Plugin} */ (reactPlugin),
+      'react-hooks': /** @type {import('eslint').ESLint.Plugin} */ (reactHooksPlugin),
+    },
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'warn',
+      '@typescript-eslint/explicit-module-boundary-types': 'warn',
+      '@typescript-eslint/no-unused-vars': ['error', {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+        destructuredArrayIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+        args: 'all'
+      }],
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unsafe-assignment': 'warn',
+      '@typescript-eslint/no-unsafe-member-access': 'warn',
+      '@typescript-eslint/no-unsafe-call': 'warn',
+    },
+    settings: {
+      react: {
+        version: 'detect',
       },
     },
   },
@@ -118,7 +153,6 @@ const config = [
   {
     files: ['**/*.astro'],
     languageOptions: {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       parser: astroPlugin.parser,
       parserOptions: {
         parser: tseslintParser,
